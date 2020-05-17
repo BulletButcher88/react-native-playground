423ec372e40af5f914958c09f8e319b96e204389
Mark Butcher <markjamesbutcher@gmail.com>
2020-05-17 23:37:12 +1000

    StyleSheet play

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -6,29 +6,51 @@ const emoji = require("emoji-dictionary");
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.text}>Child #1{emoji.getUnicode("baby")}</Text>
-      <Text style={styles.text}>Child #2{emoji.getUnicode("baby")}</Text>
-      <Text style={styles.text}>Child #3{emoji.getUnicode("baby")}</Text>
+    <View
+      style={styles.viewStyle}
+    >
+      <Text style={styles.text1}>Child #1{emoji.getUnicode("baby")}</Text>
+      <Text style={styles.text2}>Child #2{emoji.getUnicode("baby")}</Text>
+      <Text style={styles.text3}>Child #3{emoji.getUnicode("baby")}</Text>
+
     </View>)
 }
 
 
 const styles = StyleSheet.create({
-  text: {
-    marginVertical: 15,
-    marginHorizontal: 15,
+  text1: {
+    left: 30,
+    top: 200,
+    alignSelf: 'flex-start',
     fontStyle: 'italic',
-    fontSize: 20,
+    fontSize: 60,
     color: 'orange',
     borderWidth: 10,
-    borderColor: `rgb(${100}, ${230}, ${230})`
+    borderColor: `rgb(${100}, ${230}, ${230})`,
+  },
+  text2: {
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    bottom: 200,
+    fontStyle: 'italic',
+    fontSize: 60,
+    color: 'orange',
+    borderWidth: 10,
+    borderColor: `rgb(${100}, ${230}, ${230})`,
+  },
+  text3: {
+    fontStyle: 'italic',
+    fontSize: 60,
+    color: 'orange',
+    borderWidth: 10,
+    borderColor: `rgb(${100}, ${230}, ${230})`,
   },
   viewStyle: {
-    height: 300,
-    borderWidth: 4,
+    borderWidth: 20,
     borderColor: 'green',
-    alignItems: `center`
+    alignItems: `stretch`,
+    ...StyleSheet.absoluteFillObject
+
   }
 })
 
